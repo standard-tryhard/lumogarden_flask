8b010773082344f980967fb89aaa5de3383bc6a9
Arie Marie 'Thomas' Dalleis <thomasoflight@lightfaced.org>
2017-08-08 08:17:00 -0400

    Trees page added

--- routes.py
+++ routes.py
@@ -3,8 +3,20 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-	return render_template("index.html")
+def grids():
+	return render_template("grids.html")
+
+@app.route("/trees")
+def trees():
+	return render_template("trees.html")
+
+@app.route("/jars")
+def jars():
+	return render_template("jars.html")
+
+@app.route("/cards")
+def cards():
+	return render_template("cards.html")
 
 if __name__ ==  "__main__":
 	app.run(debug=True)
