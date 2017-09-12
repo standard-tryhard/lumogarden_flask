3809bc4230860fe4c23c24f38991c83b5fd54d2e
A.M. 'Thomas <thomasoflight@lightfaced.org>
2017-09-12 15:55:00 -0400

    proper use of PyMongo

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,7 @@ from whitenoise import WhiteNoise
 
 lumo_hub = Flask(__name__)
 lumo_hub.config.from_object('config')
-
-db = PyMongo(lumo_hub)
-# db = db.lumogrids_flask
-
 lumo_hub.wsgi_app = WhiteNoise(lumo_hub.wsgi_app, root='static/')
+mongo = PyMongo(lumo_hub)
 
 from app import views
