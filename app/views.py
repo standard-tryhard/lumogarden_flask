1d2b1b3e327bc089d3219d1fe767e81acce502b5
A.M. Thomas <thomasoflight@lightfaced.org>
2017-10-07 23:36:22 -0400

    Views use MongoEngine

--- app/views.py
+++ app/views.py
@@ -1,20 +1,39 @@
 from flask import render_template
 from app import lumo_hub
+from app.block import Block
 from app.forms import NewCardForm
+
 
 
 @lumo_hub.route('/')
 @lumo_hub.route('/grids/')
 def grids():
 
-    # sample_card = Card.objects[1]
+    tl = Block.objects.get(position='top_left')
+    tm = Block.objects.get(position='top_mid')
+    tr = Block.objects.get(position='top_right')
+    ml = Block.objects.get(position='mid_left')
+    mr = Block.objects.get(position='mid_right')
+    bl = Block.objects.get(position='botm_left')
+    bm = Block.objects.get(position='botm_mid')
+    br = Block.objects.get(position='botm_right')
 
-    return render_template('grids.html')
+    return render_template('grids.html',
+                           tl=tl,
+                           tm=tm,
+                           tr=tr,
+                           ml=ml,
+                           mr=mr,
+                           bl=bl,
+                           bm=bm,
+                           br=br)
 
 
 @lumo_hub.route('/jars/<string:jars_category>/')
 def jars():
-    pass
+
+
+    return render_template('jars.html')
 
 
 @lumo_hub.route('/new_card/', methods=['GET', 'POST'])
