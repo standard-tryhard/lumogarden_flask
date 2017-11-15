4560bad93a6a872235d1b473eb5ecf9874065bf2
A.M. Thomas <thomasoflight@lightfaced.org>
2017-11-15 12:50:16 -0500

    Clean up files; use scrap_views to test out card update feature

--- app/scrap_views.py
+++ app/scrap_views.py
@@ -1,78 +1,20 @@
-@lumo_hub.route('/test/<string:jar_from_url>/', methods=['GET', 'POST'])
-def test(jar_from_url):
+from flask import render_template, redirect, url_for
+from app import lumo_hub
+from app.card import Card
+from app.forms import TodoButtons
 
-    def get_jar_positions(position):
-        if Card.objects(card_in_jar=jar_from_url,
-                        card_active=position):
-
-            found_active = Card.objects(card_in_jar=jar_from_url,
-                                        card_active=position).get()
-            return found_active
-
-        else:
-            return template_card
-
-    jar_name = jar_from_url.upper()
-
-    tl = get_jar_positions('top_left')
-    tm = get_jar_positions('top_mid')
-    tr = get_jar_positions('top_right')
-    ml = get_jar_positions('mid_left')
-    mr = get_jar_positions('mid_right')
-    bl = get_jar_positions('botm_left')
-    bm = get_jar_positions('botm_mid')
-    br = get_jar_positions('botm_right')
-
-    jar_positions = {
-        'tl': tl,
-        'tm': tm,
-        'tr': tr,
-        'ml': ml,
-        'mr': mr,
-        'bl': bl,
-        'bm': bm,
-        'br': br
-        }
-
-    next_actionable_steps = {
-        'tl': get_incmplts_form(tl),
-        'tm': get_incmplts_form(tm),
-        'tr': get_incmplts_form(tr),
-        'ml': get_incmplts_form(ml),
-        'mr': get_incmplts_form(mr),
-        'bl': get_incmplts_form(bl),
-        'bm': get_incmplts_form(bm),
-        'br': get_incmplts_form(br)
-        }
-
-    form = Buttons()
-    if form.validate_on_submit():
-            # for form in next_actionable_steps.values():
-            #     print(form)
-            print('hi')
-            return redirect(url_for('testing', jar_from_url=jar_from_url))
+template_card = Card.objects(card_name='...').get()
 
 
+@lumo_hub.route('/dummy/<string:searched_card>/', methods=['GET', 'POST'])
+def show_existing_steps(searched_card):
+    searched_card = searched_card.capitalize()
 
-    return render_template('testing.html',
-                           jar_name=jar_name,
-                           jar_positions=jar_positions,
-                           next_actionable_steps=next_actionable_steps,
-                           form=form)
+    if Card.objects(card_name=searched_card):
+        found_card = Card.objects(card_name=searched_card).get()
 
+    else:
+        found_card = template_card
 
-@lumo_hub.route('/test_checkbox/', methods=['GET', 'POST'])
-def test_checkbox():
-    form = ExampleForm()
-    form.fields.choices = [('a', 'A')]
-
-    # if form.validate_on_submit():
-    #         return redirect(url_for('test_checkbox'))
-
-    return render_template('test_checkbox.html',
-                           # test_jar=test_jar,
-                           # jar_positions=jar_positions,
-                           # next_actionable_steps=next_actionable_steps,
-                           form=form)
-
-
+    return render_template('dummy_page.html',
+                           found_card=found_card)
