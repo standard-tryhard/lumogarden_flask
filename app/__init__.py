6e3fffff7b23e520b0da08011457b194d9f2b6d8
A.M. Thomas <thomasoflight@lightfaced.org>
2017-12-01 22:02:57 -0500

    Initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -5,4 +5,4 @@ lumo_hub = Flask(__name__)
 lumo_hub.config['SECRET_KEY'] = 'SECRET!'
 lumo_hub.config.from_object('config')
 from app import (blocks_view, jars_view, new_card_view,
-                 edit_card_view, card_view, scrap_views)
+                 edit_card_view, card_view, all_cards)
