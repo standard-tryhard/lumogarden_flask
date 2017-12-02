966125df1aaa943e4aa79a4188227c8363df965a
A.M. Thomas <thomasoflight@lightfaced.org>
2017-12-01 22:03:32 -0500

    clean up files

--- app/blocks_view.py
+++ app/blocks_view.py
@@ -3,36 +3,31 @@ from app import lumo_hub
 from app.block import Block
 from app.forms import TodoButtons
 
+tl = Block.objects.get(position='top_left')
+tm = Block.objects.get(position='top_mid')
+tr = Block.objects.get(position='top_right')
+ml = Block.objects.get(position='mid_left')
+mr = Block.objects.get(position='mid_right')
+bl = Block.objects.get(position='botm_left')
+bm = Block.objects.get(position='botm_mid')
+br = Block.objects.get(position='botm_right')
 
-# from app.card import Card
-# from app.card_steps import CardSteps
-# from app.data_manipulation import get_incmplts_tuple
+block_positions = {
+    'tl': tl,
+    'tm': tm,
+    'tr': tr,
+    'ml': ml,
+    'mr': mr,
+    'bl': bl,
+    'bm': bm,
+    'br': br
+}
 
 
 @lumo_hub.route('/', methods=['GET', 'POST'])
 @lumo_hub.route('/blocks/', methods=['GET', 'POST'])
 def blocks_view():
     form = TodoButtons()
-
-    tl = Block.objects.get(position='top_left')
-    tm = Block.objects.get(position='top_mid')
-    tr = Block.objects.get(position='top_right')
-    ml = Block.objects.get(position='mid_left')
-    mr = Block.objects.get(position='mid_right')
-    bl = Block.objects.get(position='botm_left')
-    bm = Block.objects.get(position='botm_mid')
-    br = Block.objects.get(position='botm_right')
-
-    block_positions = {
-        'tl': tl,
-        'tm': tm,
-        'tr': tr,
-        'ml': ml,
-        'mr': mr,
-        'bl': bl,
-        'bm': bm,
-        'br': br
-        }
 
     if form.validate_on_submit():
         return redirect(url_for('blocks_view'))
